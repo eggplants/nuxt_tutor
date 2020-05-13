be82d35c98f0e931226ea47aa6bb05affbbda7eb
eggplants <w10776e8w@yahoo.co.jp>
2020-05-13 13:44:04 +0900

    add files

---
+++ memo.md
@@ -0,0 +1,24 @@
+# memo
+- vueくんのディレクトリ構成
+- 参考: [はい]
+
+
+```text
+.
+└── my-project
+    ├── assets
+    ├── components
+    ├── layouts
+    ├── middleware
+    ├── node_modules
+    ├── pages
+    ├── plugins
+    ├── static
+    └── store
+
+10 directories
+```
+
+---
+
+[はい]: https://qiita.com/gyarasu/items/171ff1569cc71c71ac28
---
+++ my-project/pages/intro.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Intro💩duction</h1>
+    <p>{{ message }}</p>
+    <p>{{ todos }}</p>
+    <ul>
+      <li v-for="item in todos" :key="item.id">
+        {{ item.label }}
+      </li>
+    </ul>
+    <p>
+      <button @click="addTodo()">追加</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "こんにちは",
+      todos: [
+        { label: "💩", id: 0 },
+        { label: "🍆", id: 1 },
+        { label: "😀", id: 2 }
+      ]
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ label: "🍉", id: this.todos.length });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
